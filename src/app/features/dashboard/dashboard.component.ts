c6ba4fad0b551118fa12a04d83afe0a1d52aead1
inpercima <inpercima@gmail.com>
2020-03-22 11:54:19 +0100

    Refactoring methods and html structure

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Label, Color } from 'ng2-charts';
-import { ChartDataSets } from 'chart.js';
-import { DataService } from './data.service';
 import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+import { Label } from 'ng2-charts';
+import { ChartDataSets } from 'chart.js';
+
+import { DataService } from './data.service';
 
 @Component({
   selector: 'covid-dashboard',
@@ -11,94 +13,55 @@ import { DatePipe } from '@angular/common';
 })
 export class DashboardComponent implements OnInit {
 
-  lineChartDataG: ChartDataSets[];
+  chartDataGermany: ChartDataSets[];
+  chartDataSaxony: ChartDataSets[];
+  chartDataLeipzig: ChartDataSets[];
 
-  lineChartLabelsG: Label[];
+  chartLabelsGermany: Label[];
+  chartLabelsSaxony: Label[];
+  chartLabelsLeipzig: Label[];
 
-  lineChartDataS: ChartDataSets[];
-
-  lineChartLabelsS: Label[];
-
-  lineChartDataL: ChartDataSets[];
-
-  lineChartLabelsL: Label[];
-
-  pipe = new DatePipe('de-DE');
-
-  lineChartOptions = {
+  chartOptions = {
     responsive: true,
   };
+
+  pipe = new DatePipe('de-DE');
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.dataService.data('1=1').subscribe(response => {
-      const tempDataNew: any[] = [];
-      const tempDataAll: any[] = [];
-      const tempLabel: any[] = [];
-      let all = 0;
-      for (const entry of response.entries()) {
-        tempDataNew.push(entry[1]);
-        tempLabel.push(this.pipe.transform(new Date(entry[0]), 'dd.MM.yyyy'));
-        if (!tempDataAll.length) {
-          tempDataAll.push(entry[1]);
-          all = entry[1];
-        } else {
-          all += entry[1];
-          tempDataAll.push(all);
-        }
-      }
-      this.lineChartDataG = [
-        { data: tempDataNew, label: 'Novel COVID-19 cases by date in Germany' },
-        { data: tempDataAll, label: 'Novel COVID-19 cases by date in Germany cumulated' },
-      ];
-      this.lineChartLabelsG = tempLabel;
+      const data = this.prepareData(response, 'Germany');
+      this.chartDataGermany = data.get('data') as ChartDataSets[];
+      this.chartLabelsGermany = data.get('labels') as Label[];
     });
 
     this.dataService.data(`Bundesland='Sachsen'`).subscribe(response => {
-      const tempDataNew: any[] = [];
-      const tempDataAll: any[] = [];
-      const tempLabel: any[] = [];
-      let all = 0;
-      for (const entry of response.entries()) {
-        tempDataNew.push(entry[1]);
-        tempLabel.push(this.pipe.transform(new Date(entry[0]), 'dd.MM.yyyy'));
-        if (!tempDataAll.length) {
-          tempDataAll.push(entry[1]);
-          all = entry[1];
-        } else {
-          all += entry[1];
-          tempDataAll.push(all);
-        }
-      }
-      this.lineChartDataS = [
-        { data: tempDataNew, label: 'Novel COVID-19 cases by date in Saxony' },
-        { data: tempDataAll, label: 'Novel COVID-19 cases by date in Saxony cumulated' },
-      ];
-      this.lineChartLabelsS = tempLabel;
+      const data = this.prepareData(response, 'Saxony');
+      this.chartDataSaxony = data.get('data') as ChartDataSets[];
+      this.chartLabelsSaxony = data.get('labels') as Label[];
     });
 
     this.dataService.data(`IdLandkreis='14713'`).subscribe(response => {
-      const tempDataNew: any[] = [];
-      const tempDataAll: any[] = [];
-      const tempLabel: any[] = [];
-      let all = 0;
-      for (const entry of response.entries()) {
-        tempDataNew.push(entry[1]);
-        tempLabel.push(this.pipe.transform(new Date(entry[0]), 'dd.MM.yyyy'));
-        if (!tempDataAll.length) {
-          tempDataAll.push(entry[1]);
-          all = entry[1];
-        } else {
-          all += entry[1];
-          tempDataAll.push(all);
-        }
-      }
-      this.lineChartDataL = [
-        { data: tempDataNew, label: 'Novel COVID-19 cases by date in Leipzig' },
-        { data: tempDataAll, label: 'Novel COVID-19 cases by date in Leipzig cumulated' },
-      ];
-      this.lineChartLabelsL = tempLabel;
+      const data = this.prepareData(response, 'Leipzig');
+      this.chartDataLeipzig = data.get('data') as ChartDataSets[];
+      this.chartLabelsLeipzig = data.get('labels') as Label[];
     });
+  }
+
+  prepareData(response: Map<string, number>, label: string): Map<string, object> {
+    const cases: number[] = [];
+    const cumultatedCases: number[] = [];
+    const labels: Label[] = [];
+    for (const entry of response.entries()) {
+      labels.push(this.pipe.transform(new Date(entry[0]), 'dd.MM.yyyy'));
+      cases.push(entry[1]);
+      cumultatedCases.push(!cumultatedCases.length ? entry[1] : cumultatedCases[cumultatedCases.length - 1] + entry[1]);
+    }
+    const dataLabel = `Novel COVID-19 cases by date in ${label}`;
+    const result = new Map<string, object>();
+    result.set('data', [{ data: cases, label: dataLabel }, { data: cumultatedCases, label: `${dataLabel} cumulated` }]);
+    result.set('labels', labels);
+    return result;
   }
 }
--- src/app/features/dashboard/data.service.ts
+++ src/app/features/dashboard/data.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,19 +11,17 @@ export class DataService {
 
   constructor(private httpClient: HttpClient) { }
 
-  data(region: string): Observable<any> {
+  data(region: string): Observable<Map<string, number>> {
     const baseUrl = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/';
-    const extraParameters = '&outFields=AnzahlFall,Meldedatum&orderByFields=Meldedatum asc&resultType=standard';
-    return this.httpClient.get(`${baseUrl}query?f=json&where=${region}${extraParameters}`).pipe(map(response => {
-      const result = new Map();
+    const parameters = '&outFields=AnzahlFall,Meldedatum&orderByFields=Meldedatum asc&resultType=standard';
+    return this.httpClient.get(`${baseUrl}query?f=json&where=${region}${parameters}`).pipe(map(response => {
+      const result = new Map<string, number>();
       // tslint:disable-next-line: no-string-literal
       response['features'].forEach(element => {
-        const date = element.attributes.Meldedatum;
-        if (result.has(date)) {
-          result.set(date, result.get(date) + element.attributes.AnzahlFall);
-        } else {
-          result.set(date, element.attributes.AnzahlFall);
-        }
+        const attributes = element.attributes;
+        const date = attributes.Meldedatum;
+        const count = attributes.AnzahlFall;
+        result.set(date, result.has(date) ? result.get(date) + count : count);
       });
       return result;
     }));

81fac611505945832371994d4bb68b42e16f9ee2
inpercima <inpercima@gmail.com>
2020-12-29 13:27:55 +0100

    Separate cron file from http file

--- api/src/main/cron/data.php
+++ api/src/main/cron/data.php
@@ -5,13 +5,6 @@ $coreService = new CoreService();
 require_once $coreService->requireConfig();
 require_once '../service/data.service.php';
 
-$coreService->setHeader();
-
 $dataService = new DataService();
-switch ($_SERVER['REQUEST_METHOD']) {
-  default:
-  case 'GET':
-    echo $dataService->load();
-    breask;
-}
+$dataService->collect();
 ?>
